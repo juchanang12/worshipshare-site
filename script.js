ada66fa2f0942d565986a21fe31331e2c8085bf3
juchanang12 <juchanang12@gmail.com>
2025-07-26 11:17:40 +0900

    "첫 업로드: index, style, script 파일 추가

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function showSection(id) {
+  const sections = document.querySelectorAll('.section');
+  sections.forEach((section) => {
+    section.classList.remove('active');
+  });
+
+  const target = document.getElementById(id);
+  if (target) {
+    target.classList.add('active');
+  }
+}
